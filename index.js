8392666914d04e4e0ca7695469f201c706005182
Geoffrey Goodman <ggoodman@gmail.com>
2015-07-14 11:12:30 -0400

    Add hapi support. Fix regex for url rewriting

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
+var normalizeRouteRx = /^\/api\/run\/[^\/]+\/(?:[^\/\?#]*\/?)?/;
+
 function fromConnect (connectFn) {
     return function (context, req, res) {
-        req.url = req.url.replace('/api/run/', '/');
+        req.url = req.url.replace(normalizeRouteRx, '');
         req.webtaskContext = context;
         
         return connectFn(req, res);
     };
 }
 
+function fromHapi (server) {
+    var webtaskContext;
+    
+    server.ext('onRequest', function (request, response) {
+        request.setUrl(request.url.replace(normalizeRouteRx, '/'));
+        request.webtaskContext = webtaskContext;
+    });
+  
+    return function (context, req, res) {
+        var dispatchFn = server._dispatch();
+        
+        webtaskContext = context;
+        
+        dispatchFn(req, res);
+    };
+}
+
 function fromServer (httpServer) {
     return function (context, req, res) {
-        req.url = req.url.replace('/api/run/', '/');
+        req.url = req.url.replace(normalizeRouteRx, '/');
         req.webtaskContext = context;
         
         return httpServer.emit('request', req, res);
@@ -17,4 +36,5 @@ function fromServer (httpServer) {
 }
 
 exports.fromConnect = exports.fromExpress = fromConnect;
+exports.fromHapi = fromHapi;
 exports.fromServer = exports.fromRestify = fromServer;
