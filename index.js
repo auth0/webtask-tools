ab8470fef5b287c74492a8b400976c72d33fa634
Javier Centurión <javier.centurion@auth0.com>
2016-02-05 18:53:42 -0300

    Fixing bugs

--- index.js
+++ index.js
@@ -8,30 +8,30 @@ exports.fromServer = exports.fromRestify = fromServer;
 function fromConnect (connectFn) {
     return function (context, req, res) {
         var normalizeRouteRx = createRouteNormalizationRx(req.x_wt.jtn);
-        
+
         req.originalUrl = req.url;
         req.url = req.url.replace(normalizeRouteRx, '/');
         req.webtaskContext = attachStorageHelpers(context);
-        
+
         return connectFn(req, res);
     };
 }
 
 function fromHapi(server) {
     var webtaskContext;
-    
+
     server.ext('onRequest', function (request, response) {
         var normalizeRouteRx = createRouteNormalizationRx(request.x_wt.jtn);
-        
+
         request.setUrl(request.url.replace(normalizeRouteRx, '/'));
         request.webtaskContext = webtaskContext;
     });
-  
+
     return function (context, req, res) {
         var dispatchFn = server._dispatch();
-        
+
         webtaskContext = attachStorageHelpers(context);
-        
+
         dispatchFn(req, res);
     };
 }
@@ -39,11 +39,11 @@ function fromHapi(server) {
 function fromServer(httpServer) {
     return function (context, req, res) {
         var normalizeRouteRx = createRouteNormalizationRx(req.x_wt.jtn);
-        
+
         req.originalUrl = req.url;
         req.url = req.url.replace(normalizeRouteRx, '/');
         req.webtaskContext = attachStorageHelpers(context);
-        
+
         return httpServer.emit('request', req, res);
     };
 }
@@ -67,13 +67,13 @@ function attachStorageHelpers(context) {
     context.read = context.secrets.EXT_STORAGE_URL
         ?   readFromPath
         :   readNotAvailable;
-    context.read = context.secrets.EXT_STORAGE_URL
+    context.write = context.secrets.EXT_STORAGE_URL
         ?   writeToPath
         :   writeNotAvailable;
-    
+
     return context;
-    
-    
+
+
     function readNotAvailable(path, options, cb) {
         var Boom = require('boom');
 
@@ -81,20 +81,21 @@ function attachStorageHelpers(context) {
             cb = options;
             options = {};
         }
-        
+
         cb(Boom.preconditionFailed('Storage is not available in this context'));
     }
-    
+
     function readFromPath(path, options, cb) {
         var Boom = require('boom');
         var Request = require('request');
-        
+
         if (typeof options === 'function') {
             cb = options;
             options = {};
         }
-        
+
         Request({
+            uri: context.secrets.EXT_STORAGE_URL,
             method: 'GET',
             headers: options.headers || {},
             qs: { path: path },
@@ -103,11 +104,11 @@ function attachStorageHelpers(context) {
             if (err) return cb(Boom.wrap(err, 502));
             if (res.statusCode === 404 && Object.hasOwnProperty.call(options, 'defaultValue')) return cb(null, options.defaultValue);
             if (res.statusCode >= 400) return cb(Boom.create(res.statusCode, body && body.message));
-            
-            res(null, body);
+
+            cb(null, body);
         });
     }
-    
+
     function writeNotAvailable(path, data, options, cb) {
         var Boom = require('boom');
 
@@ -115,29 +116,30 @@ function attachStorageHelpers(context) {
             cb = options;
             options = {};
         }
-        
+
         cb(Boom.preconditionFailed('Storage is not available in this context'));
     }
-    
+
     function writeToPath(path, data, options, cb) {
         var Boom = require('boom');
         var Request = require('request');
-        
+
         if (typeof options === 'function') {
             cb = options;
             options = {};
         }
-        
+
         Request({
-            method: 'GET',
+            uri: context.secrets.EXT_STORAGE_URL,
+            method: 'PUT',
             headers: options.headers || {},
             qs: { path: path },
             body: data,
         }, function (err, res, body) {
             if (err) return cb(Boom.wrap(err, 502));
             if (res.statusCode >= 400) return cb(Boom.create(res.statusCode, body && body.message));
-            
-            res(null);
+
+            cb(null);
         });
     }
 }
